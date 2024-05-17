b6d2ee09863973d02f81316e3ff1f2300701aa7c
eduardobustamante <eduardobustamanteblanco@gmail.com>
2024-05-17 13:46:56 -0500

    initial commit

--- app.py
+++ app.py
@@ -3,3 +3,6 @@ import streamlit as st
 st.header('Lanzar una moneda')
 
 st.write('Esta aplicación aún no es funcional. En construcción.')
+
+# Inicie la aplicación Streamlit en el puerto 8000 (predeterminado)
+streamlit.run(app, port=8000)
