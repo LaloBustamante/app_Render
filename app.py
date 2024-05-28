fbd8eb5d98b826df6fb4e325f385204b641fb968
eduardobustamante <eduardobustamanteblanco@gmail.com>
2024-05-27 20:01:00 -0600

    another commit

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-import scipy.stats
+from scipy import stats
 import streamlit as st
 import time
 
@@ -6,22 +6,22 @@ st.header('Lanzar una moneda')
 
 chart = st.line_chart([0.5])
 
-def toss_coin(n): # función que emula el lanzamiento de una moneda
-
-    trial_outcomes = scipy.stats.bernoulli.rvs(p=0.5, size=n)
+def toss_coin(n): 
+    # Función que emula el lanzamiento de una moneda
+    trial_outcomes = stats.bernoulli.rvs(p=0.5, size=n)
 
     mean = None
     outcome_no = 0
     outcome_1_count = 0
 
     for r in trial_outcomes:
-        outcome_no +=1
+        outcome_no += 1
         if r == 1:
             outcome_1_count += 1
         mean = outcome_1_count / outcome_no
-        chart.add_rows([mean])
+        chart.add_rows([[mean]])
         time.sleep(0.05)
-
+        
     return mean
 
 number_of_trials = st.slider('¿Número de intentos?', 1, 1000, 10)
@@ -29,3 +29,5 @@ start_button = st.button('Ejecutar')
 
 if start_button:
     st.write(f'Experimento con {number_of_trials} intentos en curso.')
+    mean_result = toss_coin(number_of_trials)
+    st.write(f'Resultado promedio de lanzamientos: {mean_result}')
